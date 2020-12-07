eccde1b79ebc2a4c7b5228869b1f2b83c02c4271
ily123 <ibnovikov@gmail.com>
2020-12-07 17:21:44 -0600

    Added first complete draft of the example tab
    	- Wrote text to describe each step
    	- Created/added images for each step
    	- These images will have to be replaced
              as I refine the interface, but the basic
    	  structure is now there

--- app_text.py
+++ app_text.py
@@ -157,6 +157,8 @@ tabs = {
 
         Here is how to generate a list of these putative neigbors using my tool:
 
+        ---
+
         **Step 1: Come up with a list of known P53 kinases.**
 
         You can do it by reading literature or using a database like [Phosphosite](www.phosphosite.org). In this
@@ -170,18 +172,168 @@ tabs = {
         MAPKAPK5, NEK2, NuaK1, P38A, P38G, PAK4, PKCD, PLK3, PRPK, SMG1, Src, TAF1, VRK1
         ```
 
-        **Step 2: Enter these kinases in the diffusion form in the tool**
-        ![alt text](assets/screenshot_1.png)
+        ---
+
+        **Step 2: Enter data and conduct diffusion experiment:**
+
+        Paste the kinase list into the text box, check cross-validation, and press the
+        DIFFUSE button.
+
+        ![alt text](assets/screenshot_2.png)
+
+        ---
+
+        ** Step 3: Analyze initial results:**
+
+        The output of the diffusion experiment is the list of proteins ranked by how
+        closely they are connected to the input label set. By default all proteins
+        ranked with zscore of 2 or more are plotted on the graph (you can adjust this
+        value in the input form).
+
+        So here is what the network of our top hits looks like now:
+
+        ![alt text](assets/screenshot_3a_circle.png)
+
+        The diamonds are the orignal input labels, and circles are the top hits.
+        Color of the circle corresponds to the z-score. You can view exact ranks and
+        z-scores in the table below the graph (not shown here).
+
+        The circular layout is not super informative, so click on Force-directed layout.
+        The result will look something like this:
+
+        ![alt text](assets/screenshot_3b_force_dir.png)
+
+        The graph looks like a bit of a mess, but we begin to see some separation between
+        nodes.
+
+        However, for now, let's focus on two things:
 
 
-        * Step 3: Press diffuse, click on LOO validation flag
+        * There are some input labels not connected to anything (boxed in red).
+        Why is that? Well, the input set is pretty broad, there are multiple proteins
+        so there could be several clusters. The proteins that don't cluster to anything
+        are either under annotated, or simply don't connect well to the other labels.
 
-        * Step 4: Evaluate the results
+
+        * The metric of how good the labels connect to each other is the cross-validation
+        AUC. You can read about cross-validation in detail [here](https://en.wikipedia.org/wiki/Cross-validation_(statistics)#Leave-one-out_cross-validation).
+        We leave one of our input labels unlabeled, and diffuse the remaining N-1 labels
+        to see if the left-out label is closely connected to them. We repeat this for
+        every label, and summarize results as ROC curve. The close the are under the
+        curve, the more connected (more predictive of each other) the input labels are.
+
+        **Why do we care if the labels are connected to each other?** If they are connected
+        to each other, then our initial assumption that these labels do the same thing
+        (and can therefore predict similar things) is correct. If the labels are not
+        connected, the our input set is not informative. It's just a collection of unconnected
+        proteins!
+
+        Right now, our AUC is not very high. It's 0.61, meaning the lables are somewhat
+        connected, but there is a lot of noise. We can work with the list of the top
+        hits, but let's try to fine-tune our inputs.
+
+        ---
+
+        **Step 3b (optional): Adjust the inputs to improve cross-validation performance**
+
+        We have some unconnected proteins in the network. Let's go ahead and get rid
+        of them. In this example, those proteins are BTK, CDK9, NEK2, PAK4 and PLK3.
+        Remove them from the input set and rerun the experiment.
+
+        New input set:
+
+        ```
+        ATM, ATR, AurA, AurB, CDK1, CDK4, CDK5, Chk1, Chk2, CK1A, CK2B,
+        DAPK1, DNAPK, DYRK1A, DYRK2, ERK1, ERK2, GRK5, HIPK2, JNK1, JNK2, LKB1, LRRK2,
+        MAPKAPK5, NuaK1, P38A, P38G, PKCD, PRPK, SMG1, Src, TAF1, VRK1
+
+        ```
+        Re-run the experiment. The results will look like so:
+        ![alt text](assets/screenshot_3c_iteration1.png)
+
+        The basic connectivity of the major cluster hasn't changed, but the isolated
+        labels no longer introduce noise into the experiment. (And because we excluded
+        those labels from the analysis, the cross-validation performance of the
+        remaining labels improved to AUC=0.73.)
+
+        From here, we can continue excising proteins that are not clearly connected
+        to a cluster. Two prime targets are SRC and LRRK2. In the image below, I dragged
+        them out of the hairball to show how many connections they have:
 
 
-        Recommended:
+        ![alt text](assets/screenshot_3c_iteration2.png)
 
-        Step 5
+        They have many connections,
+        but those connections aren't very specifict. In a synthetic network
+        like mine, too many edges suggests that the proteins isn't strongly connected
+        to anything. (You can also see it in their low ranks, if you were to look in
+        the z-score table, you will find that SRC and LRRK2 are the two lowest
+        scoring labels).
+        Note: I need to think/read about hub proteins more deeply. So my explanation
+        may not be entirely corrent.
 
+        Remove these two from the set, and re-run the experiment. The input label set
+        now looks like this:
+
+        ```
+        ATM, ATR, AurA, AurB, CDK1, CDK4, CDK5, Chk1, Chk2, CK1A, CK2B,
+        DAPK1, DNAPK, DYRK1A, DYRK2, ERK1, ERK2, GRK5, HIPK2, JNK1, JNK2, LKB1,
+        MAPKAPK5, NuaK1, P38A, P38G, PKCD, PRPK, SMG1, TAF1, VRK1
+        ```
+
+        The output is now:
+
+        ![alt text](assets/screenshot_3c_iteration3_.png)
+
+        With SRC and LRRK2 gone, we can now see clear clusters in the remaining labels
+        and top hits. One last unconnected protein is GRK5. We can remove it, rerun, and
+        the final state of the experiment would be this:
+
+        ![alt text](assets/screenshot_3c_iteration4.png)
+
+
+        The cross-validation AUC is a respectable 0.87, and the hairball is gone. The
+        labels and their top hits are now interpretable by a human.
+
+        **Step 5: Interpret the results:**
+
+        Let's circle back to our original hypothesis - clusters of connected proteins
+        predict other proteins involved in the same activity by the virtue of being
+        connected. We now have a set of input labels that are connected (dimonds, AUC=0.87).
+
+        So then the top hits (proteins that were left unlabled) are our primary targets
+        for being involved in some manner with P53. All those of proteins with z-score
+        >2 are in the graph.
+
+        You can also see the table at the bottom:
+
+        ![alt text](assets/screenshot_4a.png)
+
+        Note that the labels are present in these tables (initial_state = 1). All those
+        proteins with initial_state=0 are the predictions. **To run the experiment without
+        LOO (no labels in the output table), uncked LOO box and re-run.**
+
+        You can now pass this list of top hits to your domain/experts experimentalists
+        with the following message:
+
+        ```
+        Proteins on the top of the list are likely to be associated/interact with P53 or
+        P53 related processes: HIPK1, PRKDC, TLK3, BUB1B, etc, in the order of decreasing
+        z-score.
+
+        ```
+        And you can supply them with the graph, to see if any specific clusters / associations
+        jump out.
+
+
+        **Step 6: Bonus / Loose Ends**
+
+        So what about all those labels we removed? Are they useless? Some of them are
+        (in the context of our synthetic GO term network). They are poorly annotated
+        and not connected to anything too specific.
+
+        Others are simply involved in some facet of P53 action that's not common to the
+        labels that remained in our final set. To get a better idea what those labels
+        interact with, you should diffuse them individually.
         """,
 }
